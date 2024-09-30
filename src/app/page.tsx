7db8230a6d39076472befd7ec2e8859ef367980a
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-09-30 17:42:09 -0300

    feat: create page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { Logo } from "@/components/ui/logo";
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return <h1>Olá Mundo!</h1>;
+  redirect("/home");
+  return (
+    <div
+      className="min-h-screen flex items-center justify-center
+  "
+    >
+      <Logo size={80} />
+    </div>
+  );
 }

710cb564fe35bd35a26af0ddec517062810ac199
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-09-30 17:21:40 -0300

    feat: change to relative import

--- src/components/auth/singin-form.tsx
+++ src/components/auth/singin-form.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { Input } from "../ui/input";
-import { Button } from "../ui/button";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export const SinginForm = () => {
   const router = useRouter();

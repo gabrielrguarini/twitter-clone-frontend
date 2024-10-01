25781a949f0b9a8e34a6ed21d1e01d13f6be6d03
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-09-30 21:02:58 -0300

    feat: user

---
+++ src/data/user.ts
@@ -0,0 +1,11 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+  slug: "Gabriel",
+  name: "Gabriel Guarini",
+  avatar: `https://ui-avatars.com/api/?name=Gabriel+Guarini&background=random&size=100`,
+  bio: "Uma bio de teste qualquer",
+  cover: "https://picsum.photos/300/200",
+  link: "https://gabrielrguarini.vercel.app",
+  postCount: 10,
+};
---
+++ src/types/user.ts
@@ -0,0 +1,9 @@
+export type User = {
+  slug: string;
+  name: string;
+  avatar: string;
+  bio?: string;
+  cover?: string;
+  link?: string;
+  postCount?: number;
+};

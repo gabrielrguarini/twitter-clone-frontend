6dd898b6007a9f08edb7cfc644778d575aebb7f1
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-10-02 20:22:40 -0300

    fix: button padding

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -6,7 +6,8 @@ export const Button = ({ children, onClick, size, ...props }: Props) => {
   return (
     <button
       className={`flex justify-center items-center cursor-pointer bg-white text-black font-bold rounded-3xl
-    ${size === 1 && "h-14 text-lg"}
+    px-2
+        ${size === 1 && "h-14 text-lg"}
     ${size === 2 && "h-10 text-md"}
     ${size === 3 && "h-7 text-xs"}
     ${props.className}

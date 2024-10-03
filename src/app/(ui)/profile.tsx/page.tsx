693823e7974e49eb0e68add3a9973691af038ff4
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-10-03 18:26:53 -0300

    feat: finish profile

--- src/app/(ui)/profile.tsx/page.tsx
+++ src/app/(ui)/profile.tsx/page.tsx
@@ -2,5 +2,5 @@ import { user } from "@/data/user";
 import { redirect } from "next/navigation";
 
 export default function Page() {
-  redirect(`/${user.slug}`);
+  redirect(`/${encodeURIComponent(user.slug)}`);
 }
